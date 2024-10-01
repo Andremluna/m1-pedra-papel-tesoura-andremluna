369e53bc1dfe5df03d5efec4f80349f0351a5e33
André Manoel <andremanoeel1@gmail.com>
2024-10-01 11:54:36 -0300

    Finalizado.

--- script.js
+++ script.js
@@ -1,36 +1,11 @@
-/* Desenvolva seu código abaixo */
-function playRockPaperScissor(player1, player2) {
-    var p = "Pedra";
-    var t = "Tesoura";
-    var p2 = "Papel";
-
-    if (player1 === p && player2 === p2) {
-        return "Jogador 2 venceu!";
-    } else if (player1 === p2 && player2 === t) {
-        return "Jogador 2 venceu!"
-    } else if (player1 === t && player2 === p) {
-        return "Jogador 2 venceu!"
-    } else if (player1 === p && player2 === t) {
-        return "Jogador 1 venceu!"
-    } else if (player1 === p2 && player2 === t) {
-        return "Jogador 1 venceu!"
-    } else if (player1 === p && player2 === p2) {
-        return "Jogador 1 venceu!"
-    } else if (player1 === t && player2 === p2) {
-        return "Jogador 1 venceu!"
-    } else if (player1 === p2 && player2 === p) {
-        return "Jogador 1 venceu!"
+function playRockPaperScissor(player1, player2) {    
+    if ((player1 === "Pedra" && player2 === "Tesoura") || 
+        (player1 === "Tesoura" && player2 === "Papel") || 
+        (player1 === "Papel" && player2 === "Pedra")) {
+        return "Jogador 1 venceu!";
     } else if (player1 === player2) {
-        return "Empate!"
+        return "Empate!";
+    } else {
+        return "Jogador 2 venceu!";
     }
-
 }
-
-const resultado = playRockPaperScissor("Pedra", "Tesoura");
-console.log(resultado); // Saída: "Jogador 1 venceu!"
-
-
-
-
-
-
