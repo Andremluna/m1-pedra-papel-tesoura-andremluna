3453ad8560243d8795f8781626a07519ca80fe0e
André Manoel <andremanoeel1@gmail.com>
2024-10-01 11:25:00 -0300

    Teste1

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ function playRockPaperScissor(player1, player2) {
     var p = "Pedra";
     var t = "Tesoura";
     var p2 = "Papel";
-    
-    if player1 === "Pedra" && player2 === "Papel"
-    return "Jogador 1 venceu!";
 
-    if  player1 === "Pedra" && player2 === "Tesoura"
-    return "Jogador 2 venceu!"
+    if (player1 === p && player2 === p2) {
+        return "Jogador 1 venceu!";
+    } else if (player1 === p2 && player2 === t) {
+        return "Jogador 1 venceu!"
+    } else if (player1 === p && player2 === t) {
+        return "Jogador 2 venceu!"
+    } else if (player1 === p2 && player2 === t) {
+        return "Jogador 2 venceu!"
+    } else if (player1 === p && player2 === p2) {
+        return "Jogador 2 venceu!"
+    } else if (player1 === player2) {
+        return "Empate!"
+    }
 
 }
+
+
+
+
+
+
