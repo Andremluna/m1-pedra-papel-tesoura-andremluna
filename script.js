dcca8a77c6326b9d8b8a36a6e9935ede37e2e62c
André Manoel <andremanoeel1@gmail.com>
2024-10-01 11:03:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 /* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+    var p = "Pedra";
+    var t = "Tesoura";
+    var p2 = "Papel";
+    
+    if player1 === "Pedra" && player2 === "Papel"
+    return "Jogador 1 venceu!";
+
+    if  player1 === "Pedra" && player2 === "Tesoura"
+    return "Jogador 2 venceu!"
+
+}
